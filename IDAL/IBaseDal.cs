11f379e9d58efbddea2d009a60d14c95ae64ec22
galatiayxh <1538804595@qq.com>
2020-01-10 13:35:07 +0800

    、。

--- IDAL/IBaseDal.cs
+++ IDAL/IBaseDal.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace IDAL
 {
     public interface IBaseDal<T> where T:class ,new()
     {
-        IQueryable<T> LoadEntities(System.Linq.Expressions.Expression<Func<T, bool>> whereLambda);
+        IQueryable<T> LoadEntities(Expression<Func<T, bool>> whereLambda);
         /// <summary>
         /// 分页
         /// </summary>
@@ -19,7 +20,7 @@ namespace IDAL
         /// <param name="whereLambda">查询</param>
         /// <param name="orderbyLambda">排序</param>
         /// <returns></returns>
-        IQueryable<T> LoadPageEntities<s>(int PageIndex, int PageSize, out int totalCount, System.Linq.Expressions.Expression<Func<T, bool>> whereLambda, System.Linq.Expressions.Expression<Func<T, s>> orderbyLambda, Boolean isAsc);
+        IQueryable<T> LoadPageEntities<s>(int PageIndex, int PageSize, out int totalCount, Expression<Func<T, bool>> whereLambda, Expression<Func<T, s>> orderbyLambda, Boolean isAsc);
         //添加
         T AddEntities(T entity);
         //删除
